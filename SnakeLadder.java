8c7c211ce66b5903e7ae3a96bb7ba3c5abe9bfb9
Nilesh Shinde <nileshshindes98@gmail.com>
2023-06-19 08:30:56 +0530

    ADDED: BRANCH 1(PLAYER ROLLS THE DIE BETWEEN 1 TO 6

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,6 +2,13 @@ public class SnakeLadder {
 
     public static void main(String[] args) {
         System.out.println("WELCOME TO THE SNAKE & LADDER GAME");
-        int var0 = 0;
+
         }
-    }
+        int var0 = 0;
+        int rollDie = 0;
+        public static int rollDie() {
+          int  var0 = (int) (Math.random() * 6.0D + 1.0D);
+
+            return var0;
+        }
+        }
