eb24fea6de5af988d6f13b20aa807569d36edccf
Nilesh Shinde <nileshshindes98@gmail.com>
2023-06-19 08:55:17 +0530

    ADDED: BRANCH 3 check various condition

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -18,4 +18,32 @@ public class SnakeLadder {
         int var0 = (int) (Math.random() * 3.0D + 1.0D);
         return var0;
     }
+    public static int playTurn(int var0) {
+        int var1 = rollDie();
+        int var2 = optionValue();
+        switch(var2) {
+            case 1:
+                System.out.println("No Play");
+                break;
+            case 2:
+                if (var0 + var1 > 100) {
+                    System.out.println("Sorry, you must reach exactly 100 or lower position with the die roll");
+                } else {
+                    System.out.println("Ladder, so moving ahead by die value " + var1);
+                    var0 += var1;
+                }
+                break;
+            case 3:
+                if (var0 < var1) {
+                    System.out.println("Snake, but stops at starting position");
+                    var0 = 0;
+                } else {
+                    System.out.println("Snake, moving backward by die value " + var1);
+                    var0 -= var1;
+                }
+        }
+
+        System.out.println("Player position: " + var0);
+        return var0;
+    }
 }
