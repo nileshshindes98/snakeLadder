19bcf96b2ae89bbab663da233d75e5aab9165b11
Nilesh Shinde <nileshshindes98@gmail.com>
2023-06-19 08:44:05 +0530

    ADDED: BRANCH 2

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,12 +3,19 @@ public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("WELCOME TO THE SNAKE & LADDER GAME");
 
-        }
-        int var0 = 0;
-        int rollDie = 0;
-        public static int rollDie() {
-          int  var0 = (int) (Math.random() * 6.0D + 1.0D);
+    }
 
-            return var0;
-        }
-        }
+    int var0 = 0;
+    int rollDie = 0;
+
+    public static int rollDie() {
+        int var0 = (int) (Math.random() * 6.0D + 1.0D);
+
+        return var0;
+    }
+
+    public static int optionValue() {
+        int var0 = (int) (Math.random() * 3.0D + 1.0D);
+        return var0;
+    }
+}
