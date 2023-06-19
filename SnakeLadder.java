90d0289e8fb960d1a85cc1c1a21bb174c275fdd7
Nilesh Shinde <nileshshindes98@gmail.com>
2023-06-19 08:02:11 +0530

    Player at starting point and ready to play

---
+++ SnakeLadder.java
@@ -0,0 +1,7 @@
+public class SnakeLadder {
+
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO THE SNAKE & LADDER GAME");
+        int var0 = 0;
+        }
+    }
